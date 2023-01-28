24c866a4b8f189c1adb28e5351b7aa34c343d735
Henrik Kjedsberg <henrik.kjeldsberg@live.no>
2023-01-28 15:04:36 +0100

    Test adding api docs

--- src/oasismove/__init__.py
+++ src/oasismove/__init__.py
@@ -2,7 +2,7 @@
 from importlib.metadata import metadata
 
 from .run_oasis import main
-
+from .solvers.NSfracStep import IPCS_ABCN_Move
 
 meta = metadata("oasismove")
 __version__ = meta["Version"]
@@ -10,3 +10,7 @@ __author__ = meta["Author"]
 __license__ = meta["License"]
 __email__ = meta["Author-email"]
 __program_name__ = meta["Name"]
+
+__all__ = [
+    "IPCS_ABCN_Move",
+]
