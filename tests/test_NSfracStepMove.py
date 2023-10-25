39dbbe10f3e295d487883ab3628e07c7e396ca37
Henrik Kjedsberg <henrik.kjeldsberg@live.no>
2023-10-26 00:54:20 +0200

    Add NS coupled test

--- tests/test_NSfracStepMove.py
+++ tests/test_NSfracStepMove.py
@@ -8,10 +8,19 @@ number = "([0-9]+.[0-9]+e[+-][0-9]+)"
 
 @pytest.mark.parametrize("num_processors", [1, 2])
 def test_DrivenCavity_with_NSfracStepMove(num_processors):
-    cmd = ("mpirun -np {} oasismove NSfracStepMove problem=DrivenCavity T=0.01 " +
-           "Nx=10 Ny=10 plot_interval=10000 solver={} testing=True")
-    d = subprocess.check_output(cmd.format(num_processors, "IPCS_ABCN_Move"), shell=True)
-    match = re.search("Velocity norm = " + number, str(d))
+    cmd = ["mpirun", "-np", f"{num_processors}", "oasismove", "NSfracStepMove", "problem=DrivenCavity", "T=0.01"
+        , "Nx=10", "Ny=10", "plot_interval=10000", "solver=IPCS_ABCN_Move", "testing=True"]
+
+    # Run OasisMove
+    result = subprocess.run(cmd, capture_output=True, text=True)
+
+    # Assert successful simulation
+    assert result.returncode == 0
+
+    # Get output (str)
+    output = result.stdout
+
+    match = re.search("Velocity norm = " + number, str(output))
     err = match.groups()
 
     velocity_norm = eval(err[0])
@@ -22,10 +31,19 @@ def test_DrivenCavity_with_NSfracStepMove(num_processors):
 
 @pytest.mark.parametrize("num_processors", [1, 2])
 def test_MovingVortex(num_processors):
-    cmd = ("mpirun -np {} oasismove NSfracStepMove problem=MovingVortex T=1 " +
-           "Nx=10 Ny=10 solver={}")
-    d = subprocess.check_output(cmd.format(num_processors, "IPCS_ABCN_Move"), shell=True)
-    match = re.search("Final Error: u0=" + number, str(d))
+    cmd = ["mpirun", "-np", f"{num_processors}", "oasismove", "NSfracStepMove", "problem=MovingVortex", "T=1",
+           "Nx=10", "Ny=10", "solver=IPCS_ABCN_Move"]
+
+    # Run OasisMove
+    result = subprocess.run(cmd, capture_output=True, text=True)
+
+    # Assert successful simulation
+    assert result.returncode == 0
+
+    # Get output (str)
+    output = result.stdout
+
+    match = re.search("Final Error: u0=" + number, str(output))
     err = match.groups()
     assert eval(err[0]) < 5e-3
 
